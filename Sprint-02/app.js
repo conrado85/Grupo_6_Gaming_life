72689ad2ac325738e74bdce1875d0e13ec9d4041
conrado85 <carlos_ag20@hotmail.com>
2022-12-29 18:40:24 -0300

    Cambios en Sprint2

---
+++ Sprint-02/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath =path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPath) );
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/registro.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/administrador', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/administrador.html'));
+});
+
+app.get('/compras', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/compras.html'));
+});
+
+app.get('/detalleProducto', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/detalleProducto.html'));
+});
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productos.html'));
+});
+
+app.listen(3030, () => {
+    console.log('Servidor iniciado en http://localhost:3030');
+});
