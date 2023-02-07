22970604a3505fa50627c41e9a478ca72e727960
PabloGRuiz <blitoruizrpg@gmail.com>
2023-02-06 22:03:00 -0300

    sprint4

---
+++ Sprint-4/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const mainRouter = require('./routers/main')
+
+app.use(express.static('public'));
+
+
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, './views'))
+
+app.use('/', mainRouter)
+app.use('/detalle', mainRouter)
+
+app.listen(3030, () => {
+    console.log('Servidor iniciado en http://localhost:3030');
+});
