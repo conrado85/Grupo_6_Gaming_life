8143f44875f99290964404fb5b26d0d341ee7e15
PabloGRuiz <blitoruizrpg@gmail.com>
2023-02-24 21:46:55 -0300

    cambios de estilo

---
+++ Sprint-4/routers/products.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const path = require('path')
+const productController = require('../controllers/productController')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../public/images/imgPorductos"))
+    },
+    filename: (req, file, cb) => {
+        const newFilename = "img" + Date.now + path.extname(file.originalname)
+        cb(null, "")
+    }
+})
+
+const upload = multer({storage})
+
+
+router.get('/lista', productController.productos)
+
+router.get('/detalle/:id', productController.detalleProducto)
+
+router.get('/crear' ,upload.single('imgF') ,productController.crear)
+
+router.post('/productoNuevo',productController.productoNuevo)
+
+module.exports = router
