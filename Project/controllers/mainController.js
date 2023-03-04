ac17de921d021c2e261ee3b84c5d032b3ca2f7a5
PabloGRuiz <blitoruizrpg@gmail.com>
2023-03-04 02:03:22 -0300

    cambios en 'productos'

--- Project/controllers/mainController.js
+++ Project/controllers/mainController.js
@@ -1,9 +1,6 @@
 const path = require('path')
 const fs = require('fs')
 
-const menuFile = fs.readFileSync(path.join(__dirname, '../models/productos.json'),'utf-8')
-const listaProductos = JSON.parse(menuFile)
-
 const controller = {
     index: (req, res) =>{
         res.render('home')
