84b8602bb2cbf937a3d22b3ce38d70b7e1f532f0
Emiliano Rodriguez <118319050+emilianordz@users.noreply.github.com>
2023-03-04 01:13:20 -0300

    Delete sprints directory

--- sprints/Sprint-2/public/script/menu.js
+++
@@ -1,5 +0,0 @@
-const navBarras = document.querySelector(".menu-barras")
-const navMenu = document.querySelector(".nav-menu")
-navBarras.addEventListener("click",() => {
-    navMenu.classList.toggle("nav-menu_visible");
-});
--- sprints/Sprint-4/routers/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-const mainController = require('../controllers/mainController')
-
-router.get('/inicio', mainController.index)
-
-module.exports = router
