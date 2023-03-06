fb95e14f9ada0e4443dbdcbe1266c089712fb08f
Conrado <carlos_ag20@hotmail.com>
2023-03-06 20:17:29 -0300

    cambios en la home,header,menu hamburgueza

--- Project/controllers/mainController.js
+++ Project/controllers/mainController.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-const menuFile = fs.readFileSync(path.join(__dirname, '../models/productos.json'),'utf-8')
+const menuFile = fs.readFileSync(path.join(__dirname, '../data/productos.json'),'utf-8')
 const listaProductos = JSON.parse(menuFile)
 
 const controller = {
