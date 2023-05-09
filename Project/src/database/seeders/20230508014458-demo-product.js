900a41135b66b506e99cabe967cc1741049aca7b
conrado <carlos_ag20@hotmail.com>
2023-05-08 21:48:17 -0300

    demo de los seeders usuario,productos

---
+++ Project/src/database/seeders/20230508014458-demo-product.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Products', [{
+      name: 'disco rigido',
+      description: 'disco de estado solido de 128gigas',
+      price: '200',
+      img:'',
+      category_id:'Almacenamiento',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {name: 'placa de video',
+    description: 'placade video  de 128',
+    price: '270',
+    img:'',
+    category_id:'Componente',
+    createdAt: new Date(),
+    updatedAt: new Date()
+
+    },
+    {
+      name: 'teclado',
+      description: 'teclado retroiluminado rgb',
+      price: '20',
+      img:'',
+      category_id:'Perifericos',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Products', null, {});
+  }
+};
