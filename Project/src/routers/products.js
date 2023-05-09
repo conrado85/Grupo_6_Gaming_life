acf3c772b88eb3394d053dcbc0c8177c03abd05d
Emilianordz <rodriguezeme93@gmail.com>
2023-05-08 23:49:23 -0300

    cambios en routers para session y nuevo navbar

--- Project/src/routers/products.js
+++ Project/src/routers/products.js
@@ -4,6 +4,8 @@ const multer = require('multer')
 const path = require('path')
 
 const productController = require('../controllers/productController')
+const sessionMiddleware = require("../middlewares/sessionMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware")
 
 const storage = multer.diskStorage({
   destination: (req,file,cb) => {
@@ -20,18 +22,18 @@ const upload = multer({
   storage
 })
 
-router.get('/lista', productController.productos)
+router.get('/lista', sessionMiddleware, productController.productos)
 
-router.get('/detalle/:id', productController.detalleProducto)
+router.get('/detalle/:id', sessionMiddleware, productController.detalleProducto)
 
-router.get('/crear', productController.crear)
+router.get('/crear', sessionMiddleware, authMiddleware, productController.crear)
 
 router.post('/crear', upload.single('img'),productController.new)
 
-router.get('/editar/:id', productController.edit)
+router.get('/editar/:id', sessionMiddleware, authMiddleware, productController.edit)
 
-router.put('/:id', productController.update)
+router.put('/:id', sessionMiddleware, productController.update)
 
-router.delete('/ProductoEliminado/:id', productController.delete)
+router.delete('/ProductoEliminado/:id', sessionMiddleware, authMiddleware, productController.delete)
 
 module.exports = router
