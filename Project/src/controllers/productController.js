06c7f3260ad3cf39ff68c2d6b10601f45f2d2f7c
PabloGRuiz <blitoruizrpg@gmail.com>
2023-05-15 19:37:44 -0300

    Ultimos detalles

--- Project/src/controllers/productController.js
+++ Project/src/controllers/productController.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require('fs')
 
 
-const menuFile = fs.readFileSync(path.join(__dirname, '../data/productos.json'),'utf-8')
+const menuFile = fs.readFileSync(path.join(__dirname, '../data/productos.json'), 'utf-8')
 const listaProductos = JSON.parse(menuFile)
 
 
@@ -14,57 +14,57 @@ const db = require('../database/models');
 const controller = {
 
     //renderiza la lista de productos
-    productos:async (req,res) =>{
+    productos: async (req, res) => {
         try {
-            const products =await db.Products.findAll()
-            res.render('products', {products})
+            const products = await db.Products.findAll()
+            res.render('products', { products })
         } catch (error) {
-            
+
         }
     },
 
     //renderiza el detalle del producto
-    detalleProducto:(req,res) =>{
-        db.Products.findByPk(req.params.id)
-            .then(producto => {
-                res.render('detail', {producto});
-            });
+    detalleProducto: async (req, res) => {
+        const listaProductos = await db.Products.findAll();
+        const producto = await db.Products.findByPk(req.params.id)
+        res.render('detail', { producto, listaProductos });
+
     },
 
     //renderiza el formulario de creacion de un producto
-    crear:async(req,res) =>{
+    crear: async (req, res) => {
         const categories = await db.Category.findAll()
-        res.render('product-create',{categories})
+        res.render('product-create', { categories })
     },
 
     //crea un nuevo producto
-    new: async(req,res) => {
-        try{
+    new: async (req, res) => {
+        try {
             const product = {
                 name: req.body.name,
                 price: req.body.price,
                 description: req.body.description,
                 category: req.body.category,
-                img: req.file? req.file.filename: 'default-image.png'
+                img: req.file ? req.file.filename : 'default-image.png'
             }
             await db.Products.create(product)
             res.redirect('/gaminglife/productos/lista')
-        }catch{
+        } catch {
             res.send(error)
         }
     },
 
     //renderiza la vista de editar producto
-    edit:async(req,res)=>{
+    edit: async (req, res) => {
         const categories = await db.Category.findAll()
         db.Products.findByPk(req.params.id)
             .then(productToEdit => {
-                res.render('product-edit', {productToEdit, categories} );
-            });     
+                res.render('product-edit', { productToEdit, categories });
+            });
     },
 
     //actualiza un producto
-    update:(req,res)=>{
+    update: (req, res) => {
         let productId = req.params.id;
         db.Products.update(
             {
@@ -72,21 +72,31 @@ const controller = {
                 price: req.body.price,
                 description: req.body.description,
                 category: req.body.category,
+            }, {
+            where: {
+                id: productId
+            }
+        }
+        )
+            .then(() => {
+                return res.redirect('/gaminglife/productos/lista')
             })
-        .then(()=> {
-            return res.redirect('/gaminglife/productos/lista')})            
-        .catch(error => res.send(error))
+            .catch(error => {
+                res.send(error)
+                console.log(error)
+            })
     },
     //borra un producto
-    delete:(req,res)=>{
+    delete: (req, res) => {
         let productId = req.params.id;
-        db.Products.destroy({where: {id: productId}, force: true}) // force: true es para asegurar que se ejecute la acción
-        .then(()=>{
-            return res.redirect('/gaminglife/productos/lista')})
-        .catch(error => res.send(error)) 
+        db.Products.destroy({ where: { id: productId }, force: true }) // force: true es para asegurar que se ejecute la acción
+            .then(() => {
+                return res.redirect('/gaminglife/productos/lista')
+            })
+            .catch(error => res.send(error))
     }
 
-    
+
 }
 
 module.exports = controller
