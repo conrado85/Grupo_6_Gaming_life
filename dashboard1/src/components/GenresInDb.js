84940c117590a131dc46825b1285ac12c628f78d
Maxi Medina <maximilianomr11.mmr@gmail.com>
2023-05-17 20:41:26 -0300

    Categorias

--- dashboard1/src/components/GenresInDb.js
+++ dashboard1/src/components/GenresInDb.js
@@ -2,48 +2,37 @@ import React, { Component } from 'react';
 import Genre from './Genre';
 
 
-class GenresInDb extends Component {
-    constructor() {
-        super()
-        this.state = {
-            genres: []
-        }
-    }
-
-    async componentDidMount() {
-        const response = await fetch("/api/products");
-        const data = await response.json();
-        this.setState({ genres: data.products });
-       console.log(data)
-    }
-
-    SecondaryColor() {
-        document.querySelector("#card-body").classList.toggle("bg-secondary")
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                {/*<!-- Categories in DB -->*/}
-                <div className="col-lg-6 mb-4">
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 onMouseOver={this.SecondaryColor} className="m-0 font-weight-bold text-gray-800">Categorias</h6>
-                        </div>
-                        <div id="card-body" className="card-body">
-                            <div className="row">
-                                {
-                                    this.state.genres.map((genre, index) => {
-                                        return <Genre  {...genre} key={genre + index} />
-                                    })
-                                }
-                            </div>
-                        </div>
-                    </div>
+function GenresInDb() {
+    return (
+      <div className="col-lg-6 mb-4">
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h5 className="m-0 font-weight-bold text-gray-800">
+              Categorias
+            </h5>
+          </div>
+          <div className="card-body">
+            <div className="row">
+              <div className="col-lg-6 mb-4">
+                <div className="card bg-dark text-white shadow">
+                  <div className="card-body">Periferico</div>
                 </div>
-
-            </React.Fragment>
-        )
-    }
-}
+              </div>
+  
+              <div className="col-lg-6 mb-4">
+                <div className="card bg-dark text-white shadow">
+                  <div className="card-body">Componentes</div>
+                </div>
+              </div>
+              <div className="col-lg-6 mb-4">
+                <div className="card bg-dark text-white shadow">
+                  <div className="card-body">Almacenamiento</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 export default GenresInDb;
