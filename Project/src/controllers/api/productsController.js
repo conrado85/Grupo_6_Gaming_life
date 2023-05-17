98d0f60048178ef6333c7bf137ddbe32140767ed
Maxi Medina <maximilianomr11.mmr@gmail.com>
2023-05-17 20:36:44 -0300

    proyecto terminado

--- Project/src/controllers/api/productsController.js
+++ Project/src/controllers/api/productsController.js
@@ -1,63 +1,69 @@
 const db = require('../../database/models');
-const { error, count } = require('console');
-const { response } = require('express');
 
 const controller = {
-    list: async (req, res) => {
-        try {
-            const products = await db.Products.findAll({
-                include: [{ model: db.Category, as: 'category' }]
-              });
-            const countByCategory = {};
-            products.forEach((product) => {
-                const categoryId = product.category ? product.category.name : null;
-                if (countByCategory[categoryId]) {
-                    countByCategory[categoryId]++;
-                } else {
-                    countByCategory[categoryId] = 1;
-                }
-            })
-            const response = {
-                count: products.length,
-                countByCategory: countByCategory,
-                products: products.map((product) => ({
-                    id: product.id,
-                    name: product.name,
-                    description: product.description,
-                    categories: product.category ? product.category.name : null,
-                    detail: `http://localhost:3030/products/${product.id}`,
-                })),
-            };
+  list: async (req, res) => {
+    try {
+      const products = await db.Products.findAll({
+        include: [{ model: db.Category, as: 'category' }]
+      });
 
-            res.json(response);
-
-        } catch (error) {
-            console.log(error)
-            res.send(error)
+      const countByCategory = {};
+      products.forEach((product) => {
+        const categoryId = product.category ? product.category.name : null;
+        if (countByCategory[categoryId]) {
+          countByCategory[categoryId]++;
+        } else {
+          countByCategory[categoryId] = 1;
         }
-    },
-    getByID: async (req, res) => {
-        try {
-            const products = await db.Products.findByPk(req.params.id);
-            if (!products) {
-                return res.status(404).send({
-                    status: 404,
-                    error: 'producto no encontrado'
+      });
 
-                })
-            }
+      const response = {
+        count: products.length,
+        countByCategory: countByCategory,
+        products: products.map((product) => ({
+          id: product.id,
+          name: product.name,
+          description: product.description,
+          categories: product.category ? product.category.name : null,
+          detail: `http://localhost:3030/products/${product.id}`,
+          imageUrl: `http://localhost:3030/images/imgProductos/${product.img}`,
+        })),
+      };
 
-            const response = {
-                name: products.name,
-                description: products.description,
-                categories: product.category ? product.category.name : null,
-                imageUrl: `https://localhost:3030/images/${req.params.id}.jpg`
-            };
-            res.json(response);
-        } catch (error) {
-            res.send(error)
-        }
+      res.json(response);
+    } catch (error) {
+      console.log(error);
+      res.send(error);
     }
-}
+  },
+
+  getByID: async (req, res) => {
+    try {
+      const product = await db.Products.findByPk(req.params.id);
+      if (!product) {
+        return res.status(404).send({
+          status: 404,
+          error: 'Producto no encontrado',
+        });
+      }
+
+      const response = {
+        productos: [
+          {
+            
+            name: product.name,
+            description: product.description,
+            categories: product.category_id,
+            imageUrl: `http://localhost:3030/images/imgProductos${product.img}`,
+          },
+        ],
+      };
+
+      res.json(response);
+    } catch (error) {
+      res.send(error);
+    }
+  },
+};
 
 module.exports = controller;
--- dashboard1/src/components/LastMovieInDb.js
+++ dashboard1/src/components/LastMovieInDb.js
@@ -1,28 +1,56 @@
-import React from 'react';
-import imagenFondo from '../assets/images/DiscodurointernoSeagateSkyHawk1TB.jpg';
+import React, { useState, useEffect } from 'react';
 
-function LastMovieInDb(){
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Ultimo producto visto</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-                    </div>
-                    <p> DISCOS RIGIDOS 2,5¨ 160 GB -
+function LastMovieInDb() {
+  const [lastProduct, setLastProduct] = useState(null);
 
-Marca: Samsung -WD -Toshiba - (Segun Stock)
+  useEffect(() => {
+    const fetchLastProduct = async () => {
+      try {
+        const response = await fetch('/api/products');
+        const data = await response.json();
+        const products = data.products;
+        if (products.length > 0) {
+          const lastProduct = products[products.length - 1];
+          console.log(lastProduct)
+          setLastProduct(lastProduct);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-PARA USAR EN NETBOOKS, NOTEBOOKS, PC, All in one, Macbook - DVR
-PLAY STATION 3 y 4 todos los modelos (No incluye soporte)</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-                </div>
-            </div>
+    fetchLastProduct();
+  }, []);
+
+  return (
+    <div className="col-lg-6 mb-4">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5 className="m-0 font-weight-bold text-gray-800">Ultimo producto visto</h5>
         </div>
-    )
+        <div className="card-body">
+          {lastProduct ? (
+            <>
+              <div className="text-center">
+                <img
+                  className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                  style={{ width: '40rem' }}
+                  src={lastProduct.imageUrl}
+                  alt={lastProduct.name}
+                />
+              </div>
+              <p>{lastProduct.description}</p>
+              <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">
+                Ver detalles del producto
+              </a>
+            </>
+          ) : (
+            <p>No se encontraron productos</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LastMovieInDb;
